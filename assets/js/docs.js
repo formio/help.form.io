2dc12a31904b5db9298b0b6f6c175155daeba4e6
Randall Knutson <randall@form.io>
2015-07-07 11:20:48 -0500

    Add anchorjs.

---
+++ assets/js/docs.js
@@ -0,0 +1,6 @@
+/** Add anchor links to anchor tags **/
+'use strict';
+(function () {
+    anchors.options.placement = 'left';
+    anchors.add('.docs-section > h1, .docs-section > h2, .docs-section > h3, .docs-section > h4, .docs-section > h5')
+})();
